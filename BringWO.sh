ea6a4c3a6511ca5358498eb97a8503cacb88c6ff
Rafael <barto_et@hotmail.com>
2016-11-01 17:48:47 -0600

    Update BringWO.sh

--- BringWO.sh
+++ BringWO.sh
@@ -1,2 +1,9 @@
-/bin/bash!
-#
+#!/bin/bash
+#DoS Wifi by Overlord
+echo Bringing Online Wifi Weapons
+#This will check the wireless interfaces
+wlan = $(ifconfig | grep wlan)
+if wlan == null then
+echo "There's no wifi on this machine dude"
+else 
+for i in {0..10}; do ifconfig wlan${i} up; done
